b45108324080b726f79b644b63c075e0f185c35c
Joris MILLOT <jmillot@cisa-informatique.com>
2025-06-11 10:18:26 +0200

    Ajout de joris

---
+++ ConsoleApp1/Joris/Joris.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Joris
+{
+    public class Joris
+    {
+        public void DireBonjour()
+        {
+            Console.WriteLine("Bonjour, je suis Joris !");
+        }
+    }
+}
